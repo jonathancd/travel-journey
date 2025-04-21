9c0587b0e18caa036031d020d935f7f8f9319bbc
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-04-22 00:23:37 +0400

    fix scroll x in slide layout

--- src/layouts/SlideLayout.tsx
+++ src/layouts/SlideLayout.tsx
@@ -1,13 +1,11 @@
-
 import { PageTitle } from "../components/utils/PageTitle";
 import { SlideLayoutProps } from "../types/SlideLayoutProps";
 
-export const SlideLayout = ({title, children}: SlideLayoutProps) => {
-    return (
-        <div className="h-full w-full flex flex-row">
-             {/* justify-end */}
-            <PageTitle name={title} />
-            {children}
-        </div>
-    );
-}
+export const SlideLayout = ({ title, children }: SlideLayoutProps) => {
+  return (
+    <div className="h-full w-full flex flex-row overflow-x-hidden">
+      <PageTitle name={title} />
+      {children}
+    </div>
+  );
+};
