94eaec00578203bdf9950b9bf0d2f9793f53c9f7
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-04-22 22:35:58 +0400

    feat, closed: change the bottom of the instagram link

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,38 +1,43 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { Link, Outlet } from "react-router-dom";
-import { LangButton } from '../components/utils/LangButton';
+import { LangButton } from "../components/utils/LangButton";
 
 export const MainLayout = () => {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const currentLang = i18n.language;
 
   const changeLanguage = (lang: string) => {
-   i18n.changeLanguage(lang);
-   localStorage.setItem('i18nextLng', lang); // opcional: aseguramos persistencia
+    i18n.changeLanguage(lang);
+    localStorage.setItem("i18nextLng", lang); // opcional: aseguramos persistencia
   };
 
-  const languages = ['en', 'es'];
+  const languages = ["en", "es"];
 
   return (
     <div className="h-screen overflow-hidden relative">
-        <nav className="fixed top-0 left-0 w-full z-50 bg-white/70 backdrop-blur shadow flex items-center justify-between py-4 px-16">
-            <Link to="/" className="text-xl font-bold text-gray-800">
-                Travel Journey
-            </Link>
-            <div className="space-x-2">
-              { languages.map((lang) => (
-                <LangButton key={lang} lang={lang} currentLang={currentLang} onClick={changeLanguage}/>
-              ))}
-            </div>
-        </nav>
-        <div>
-          <Outlet />
+      <nav className="fixed top-0 left-0 w-full z-50 bg-white/70 backdrop-blur shadow flex items-center justify-between py-4 px-16">
+        <Link to="/" className="text-xl font-bold text-gray-800">
+          Travel Journey
+        </Link>
+        <div className="space-x-2">
+          {languages.map((lang) => (
+            <LangButton
+              key={lang}
+              lang={lang}
+              currentLang={currentLang}
+              onClick={changeLanguage}
+            />
+          ))}
         </div>
-        <footer className="fixed bottom-0 w-full px-16">
-          <div>
-            <Link to="/">Instagram</Link>
-          </div>
-        </footer>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
+      <footer className="fixed bottom-6 w-full px-16">
+        <div>
+          <Link to="/">Instagram</Link>
+        </div>
+      </footer>
     </div>
   );
 };
