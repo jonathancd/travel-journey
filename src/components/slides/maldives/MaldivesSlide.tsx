30481b531f8096fb17b3279f34b64f27db19d28e
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-04-25 16:01:18 +0400

    feat, closed: Maldives slide

--- src/components/slides/maldives/MaldivesSlide.tsx
+++ src/components/slides/maldives/MaldivesSlide.tsx
@@ -1,5 +1,54 @@
-export const MaldivesSlide = () => (
-    <div className="h-screen w-screen bg-cover bg-center flex flex-col justify-center items-center text-white text-center">
-        Maldives
-    </div>
-)
+import { useTranslation } from "react-i18next";
+import { Tab } from "@headlessui/react";
+import { SlideTabsLayout } from "../../../layouts/SlideTabsLayout";
+import { MaldivesData as tabs } from "../../../data/maldivesData";
+
+export const MaldivesSlide = () => {
+  const { t } = useTranslation();
+
+  return (
+    <SlideTabsLayout
+      pageTitle="Perfect Honeymoon"
+      tabs={tabs}
+      showTabs={false}
+      paginationOnLeft={false}
+    >
+      {({ currentTab, currentItem }) => (
+        <div className="flex flex-row h-full w-full">
+          <Tab.Panels className="w-[40vw] flex justify-start h-[100%]">
+            {tabs.map((tab) => (
+              <Tab.Panel
+                key={tab.id}
+                className="w-full mr-[10%] bg-cover bg-center bg-no-repeat"
+                style={{
+                  backgroundImage: `url('${currentItem.img}')`,
+                }}
+              />
+            ))}
+          </Tab.Panels>
+          <div className="h-full w-[60%] flex flex-col justify-start items-start">
+            <p>
+              {currentTab?.name} {currentItem.id}/{currentTab?.items.length}
+            </p>
+            <h2 className="text-7xl">
+              {t(
+                `${currentTab?.code.toLocaleLowerCase()}.items.${
+                  currentItem.code
+                }.title`
+              )}
+            </h2>
+            <p className="text-sm whitespace-normal font-light mt-[20px]">
+              <strong>
+                {t(
+                  `${currentTab?.code.toLocaleLowerCase()}.items.${
+                    currentItem.code
+                  }.description`
+                )}
+              </strong>
+            </p>
+          </div>
+        </div>
+      )}
+    </SlideTabsLayout>
+  );
+};
---
+++ src/data/maldivesData.ts
@@ -0,0 +1,31 @@
+import { TabData } from "../types/Tabs";
+
+export const MaldivesData: TabData[] = [
+  {
+    id: 1,
+    name: "Maldives",
+    code: "maldives",
+    items: [
+      {
+        id: 1,
+        code: "item1",
+        img: "/images/maldives/maldives1.jpg",
+      },
+      {
+        id: 2,
+        code: "item2",
+        img: "/images/maldives/maldives2.jpg",
+      },
+      {
+        id: 3,
+        code: "item3",
+        img: "/images/maldives/maldives3.jpg",
+      },
+      {
+        id: 4,
+        code: "item4",
+        img: "/images/maldives/maldives4.jpg",
+      },
+    ],
+  },
+];
