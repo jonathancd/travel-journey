ed69845779bb42543c92a95b802e853e6f7903e7
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-05-07 15:25:18 +0400

    feat, closed: github pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/travel-journey/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -13,4 +14,4 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-})
+});
