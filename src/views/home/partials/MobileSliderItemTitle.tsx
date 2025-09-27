5ea7b2b1b7f492296fb569e52949923efb151f10
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-09-27 12:52:08 +0400

    Feat, update the slider title in mobile

--- src/views/home/partials/MobileSliderItemTitle.tsx
+++ src/views/home/partials/MobileSliderItemTitle.tsx
@@ -2,6 +2,6 @@ import { PageTitle } from "../../../types/MobileSlider";
 
 export const MobileSliderItemTitle = ({ title }: PageTitle) => (
   <div className="h-[5vh] px-4">
-    <h3 className="text-lg font-bold">{title}</h3>
+    <h3 className="text-center text-2xl font-bold">{title}</h3>
   </div>
 );
